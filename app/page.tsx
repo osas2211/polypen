6fada5616a732debe6d0b0361913c559381229b6
Osaretin <osaretin.frank10@gmail.com>
2025-07-05 22:32:45 +0100

    add updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,72 @@
-import Image from "next/image";
+"use client"
+
+import { ShimmerButton } from "@/components/magicui/shimmer-button"
+import { ShinyButton } from "@/components/magicui/shiny-button"
+
+const images_1 = ["/landing/developer.avif", "/landing/poet.avif"]
+const images_2 = ["/landing/student.avif", "/landing/writer.avif"]
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="bg-[#fffefd] h-screen overflow-hidden">
+      <div className="md:grid md:grid-cols-2 gap-4 font-sans md:w-[90%] mx-auto px-4">
+        <div className="flex flex-col items-center justify-center md:h-screen">
+          <div className="md:py-[4rem] py-2">
+            <p className="px-4 md:px-4 py-2 bg-gray-100 inline-block rounded-full font-medium text-purple-600">
+              #CustomHack
+            </p>
+            <h3 className="md:text-9xl font-semibold">PolyPen</h3>
+            <div className="md:mt-[5rem] md:mb-[2.5rem] md:max-w-[70%]">
+              <p className="text-2xl mb-3 md:mb-3 font-medium">
+                Real-Time Multilingual Collaborative Writing that translats both
+                language and intent.
+              </p>
+              <p className="text-gray-500">
+                Whether a developer is co-writing with a product manager or a
+                songwriter, it adapts tone, role, and context in real time.
+                It&apos;s not just a writing tool, it&apos;s a handshake between
+                every kind of writer.
+              </p>
+            </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div className="flex md:flex-row flex-col gap-4 max-w-[550px]">
+              <ShimmerButton className="rounded-full w-full h-[50px]">
+                Get Started
+              </ShimmerButton>
+              <ShinyButton className="rounded-full w-full h-[50px]">
+                Explore Docs
+              </ShinyButton>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="grid grid-cols-2 gap-5 relative ">
+          <div className="space-y-5">
+            {images_2.map((image, index) => {
+              return (
+                <div key={index}>
+                  <img
+                    src={image}
+                    className="h-[60vh] w-full object-cover object-top"
+                  />
+                </div>
+              )
+            })}
+          </div>
+          <div className="space-y-5  -mt-[10rem]">
+            {images_1.map((image, index) => {
+              return (
+                <div key={index}>
+                  <img
+                    src={image}
+                    className="h-[60vh] w-full object-cover object-top"
+                  />
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
